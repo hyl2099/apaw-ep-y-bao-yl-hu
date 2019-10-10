3dd1518c2ff9d01a70be81b2e70ea4bd197ed9d7
Buyen <amguulan127@gmail.com>
2019-10-10 19:01:19 +0200

    add Swagger--BaoYing

---
+++ src/main/java/es/upm/miw/apaw_ep_themes/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package es.upm.miw.apaw_ep_themes;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration("SwaggerConfig")
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("Swagger构建Restful API文档")
+                .description("文档除了查看API还可以进行调试")
+                .version("1.0.0")
+                .build();
+
+    }
+}
