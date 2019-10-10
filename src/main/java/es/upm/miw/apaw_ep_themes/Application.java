f1366c2988418315292b77b2e80f211d8ee4a133
Buyen <amguulan127@gmail.com>
2019-10-10 18:58:48 +0200

    add Swagger--BaoYing

--- src/main/java/es/upm/miw/apaw_ep_themes/Application.java
+++ src/main/java/es/upm/miw/apaw_ep_themes/Application.java
@@ -3,8 +3,11 @@ package es.upm.miw.apaw_ep_themes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class}) // Not API: /error
+@EnableSwagger2
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);// mvn clean spring-boot:run
